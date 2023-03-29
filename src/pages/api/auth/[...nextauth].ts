92f7db04895060d43d79791cc48301f45fd73fa9
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-03-29 13:44:31 +0100

    setup discord provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
   providers: [
@@ -19,6 +20,10 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
     }),
   ],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,21 +12,22 @@ export default function Home() {
   console.log(session);
   return (
     <>
-      <h1 className="text-red-700 font-bold ">{session?.user?.name}</h1>
-      <img
-        src={session?.user?.image!}
-        alt=""
-        className="w-[128px] h-32 rounded-full"
-      />
-      {session ? (
-        <button onClick={() => signOut()} className="bg-blue-400">
-          Sign out
-        </button>
-      ) : (
-        <button onClick={() => signIn()} className="bg-blue-400">
-          Sign in
-        </button>
-      )}
+      <div className="h-screen w-screen flex items-center justify-center">
+        {session ? (
+          <div className=" flex flex-col gap-1 items-center  ">
+            <h2>{session?.user?.name}</h2>
+            <img
+              src={session?.user?.image!}
+              alt=""
+              className="w-32 h-32 rounded-full"
+            />
+            <h4>{session?.user?.email}</h4>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <button onClick={() => signIn()}>Sign in</button>
+        )}
+      </div>
     </>
   );
 }
